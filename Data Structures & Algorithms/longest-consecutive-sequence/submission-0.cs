7576104e6c32d0898c5778347903412313173d48
Sujit Phadtare <107470626+SujitPhadtare@users.noreply.github.com>
2026-05-22 15:31:32 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        int count = 1;
+        int maxCount = 1;
+        Array.Sort(nums);
+        for(int i = 0;i<nums.Length-1;i++){
+            if(nums[i]+1 == nums[i+1]){
+                count++;
+            }else if(nums[i] == nums[i+1]){
+
+            }else{
+                if(maxCount < count){
+                    maxCount = count;
+                }
+                count = 1;
+            }
+        }
+                if(maxCount < count){
+                    maxCount = count;
+                }
+        return maxCount;
+    }
+}
