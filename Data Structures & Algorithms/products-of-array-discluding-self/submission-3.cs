ac5a9a43ba5fa36f6580ef3a9f9e9c4f547f1ad7
Sujit Phadtare <107470626+SujitPhadtare@users.noreply.github.com>
2026-05-22 13:43:32 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+       int len = nums.Length;
+        
+        int[] r = new int[len];
+        int[] res = new int[len];
+
+        for(int i = 0; i< len; i++){
+            if(i != 0){
+                leftCal(i,res, nums[i-1]);
+            }else{
+                res[i] = 1;
+            }
+
+            int j = len-1 - i;
+
+            if(j != len-1){
+                 rightCal(j, r,nums[j+1]);
+            }else{
+                r[j] = 1 ;
+            }
+        }
+
+        for(int i=0; i<len;i++){
+            res[i] = res[i]*r[i];
+        }
+        return res;
+
+    }
+
+    public void leftCal(int i, int[] l,int num){
+        l[i] = l[i-1]*num;
+    }
+
+    public void rightCal(int i, int[] r,int num){
+        r[i] = r[i+1]*num;
+    }
+}
